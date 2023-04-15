129246e8efd24a1890493404cdea350de90b2329
Rosalie Newman <rosalienewman0@gmail.com>
2023-04-15 19:01:33 -0400

    first commit

---
+++ app/root.tsx
@@ -0,0 +1,34 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import type { MetaFunction, LinksFunction } from "@remix-run/node";
+import styles from "./styles/app.css";
+
+// Export a function named links, which follows a convention Remix picks up on and uses to import assets into all modules.
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: styles }];
+};
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
